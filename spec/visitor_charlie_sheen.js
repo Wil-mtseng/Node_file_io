6658520b0cd320e0b5e5c2efbab092a0655ace29
Wil-mtseng <55979402+Wil-mtseng@users.noreply.github.com>
2020-02-03 13:55:43 +0200

    Create visitor_charlie_sheen.js

---
+++ spec/visitor_charlie_sheen.js
@@ -0,0 +1,8 @@
+{
+    "fullName": "charlie_sheen",
+    "age": 55,
+    "dateOfVisit": "1965 Sept 3",
+    "timeOfVisit": "08:20am",
+    "comments": "I am an American actor!",
+    "nameOfPersonAssisting": "Wilfred"
+}
